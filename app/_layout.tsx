2537fd4d7168c13ec09ccbba7c5e388931c21ffd
CharlotteYDE <cph-cy36@cphbusiness.dk>
2025-05-12 11:32:37 +0200

    InitialCommit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,34 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Feather } from '@expo/vector-icons';
+import { Stack, router } from "expo-router";
+import { Pressable } from 'react-native';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  <Stack>
+    <Stack.Screen name="index"
+    options={{
+      headerShown: false,
+    }} />
+    <Stack.Screen name="profile"
+    options={{
+      headerTitle: "Profil",
+      headerLeft: () => (
+        <Pressable 
+          style={{ 
+            marginLeft: 0,
+            marginTop: 8,
+            marginBottom: 8,
+            width: 44,
+            height: 44,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+          onPress={() => router.back()}
+        >
+          <Feather name="chevron-left" size={24} color="#051B2F" />
+        </Pressable>
+      ),
+    }}
+     />
+  </Stack>);
 }
